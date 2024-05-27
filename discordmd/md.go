96fcee6600019b144d01c42dd2641cccc22781dc
diamondburned <diamond@libdb.so>
2024-05-27 15:55:32 -0700

    Remove obnoxious parsing log

--- discordmd/md.go
+++ discordmd/md.go
@@ -1,8 +1,6 @@
 package discordmd
 
 import (
-	"log"
-
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/state/store"
 	"github.com/yuin/goldmark/ast"
@@ -37,8 +35,6 @@ func ParseWithMessage(b []byte, s store.Cabinet, m *discord.Message, msg bool) a
 		parser.WithBlockParsers(BlockParsers()...),
 		parser.WithInlineParsers(inlineParsers...),
 	)
-
-	log.Printf("Parsing content: %q", string(b))
 
 	return p.Parse(text.NewReader(b), parser.WithContext(ctx))
 }
